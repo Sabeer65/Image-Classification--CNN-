c325062b855827f4cf33e4168f4110ee50187a12
Sabeer <sabeer.h4774@gmail.com>
2025-12-29 23:01:38 +0530

    dataset creation and numpy iterator

---
+++ test.py
@@ -0,0 +1,19 @@
+def can_harvest():
+    pass
+
+def get_world_size():
+    return 3
+
+def harvest():
+    pass
+
+plant)
+
+
+
+while True:
+    for i in range(get_world_size()):
+        if can_harvest():
+            harvest()
+        plant(Entities.Bush)
+        if i <
